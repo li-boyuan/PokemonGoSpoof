0baf96256c345e922baa9d760a22254168ae3320
Boyuan Li <liboyuan@amazon.com>
2020-12-06 11:49:22 -0800

    Add GPX files

---
+++ PokemonGoSpoof/PokemonGoSpoof/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonGoSpoof
+//
+//  Created by Li, Boyuan on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
